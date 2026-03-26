a7d8259980b2f8281dc2290937b96adad9193638
edithmuz <255868811+edithmuz@users.noreply.github.com>
2026-03-26 19:48:30 +0200

    Added basic form function and Typewriter plugin

--- src/script.js
+++ src/script.js
@@ -1,1 +1,13 @@
+function generateRecipe(event) {
+  event.preventDefault();
 
+  new Typewriter("#recipe", {
+    strings: "Vanilla sponge cake recipe",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let recipeGeneratorForm = document.querySelector("#recipe-generator-form");
+recipeGeneratorForm.addEventListener("submit", generateRecipe);
