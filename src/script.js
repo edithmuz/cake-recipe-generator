dddf059a517e8102321f10cf17d2c3a43c5302c4
edithmuz <255868811+edithmuz@users.noreply.github.com>
2026-04-01 11:21:41 +0200

    Added User Experience and changed background

--- src/script.js
+++ src/script.js
@@ -15,12 +15,12 @@ function generateRecipe(event) {
   let apiKey = "at0a18b31ff9827b40242c3f5270f8ob";
   let prompt = `User instructions: Generate a recipe for ${userInput.value}`;
   let context =
-    "You are professional baker and you like to share some to share some cake recipes. Generate a short cake recipe with ingredients according to user request below. Your response should be in basic <HTML> with headings in bold and each line separated with <br />. Do not include html and punctuation mark at beginning and at the end of recipe. Make the headings bold";
+    "You are professional baker and you like to share some to share some cake recipes. Generate a short cake recipe with ingredients according to user request below. Your response should be in basic <HTML> with headings in BOLD colour and each line separated with <br />. Do not include html and punctuation mark at beginning and at the end of recipe. Make the headings bold";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  console.log("Generating recipe");
-  console.log("Prompt:${prompt}");
-  console.log("Context:${context}");
+  let recipeElement = document.querySelector("#recipe");
+  recipeElement.classList.remove("hidden");
+  recipeElement.innerHTML = `⌛Generating a recipe for ${userInput.value}...`;
 
   axios.get(apiUrl).then(displayRecipe);
 }
